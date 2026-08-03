d50bb79eff4c39ffbfb9c4df957ca3ec5c93fd1c
adamahmadadam7046-commits <adamahmadadam7046@gmail.com>
2026-08-03 09:56:32 +0100

    Add files via upload
    First website upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card,index)=>{
+card.style.opacity="0";
+card.style.transform="translateY(30px)";
+
+setTimeout(()=>{
+card.style.transition="0.6s";
+card.style.opacity="1";
+card.style.transform="translateY(0)";
+},index*200);
+
+});
+
+const button=document.querySelector(".btn");
+
+if(button){
+
+button.addEventListener("mouseover",()=>{
+button.style.transform="scale(1.08)";
+});
+
+button.addEventListener("mouseout",()=>{
+button.style.transform="scale(1)";
+});
+
+}
+
+});
